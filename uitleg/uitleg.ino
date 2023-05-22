76f900b077d52c540c4a49bc77ddf6791ad7b6df
Cybul <gabrielcybulski@gmail.com>
2023-05-22 15:09:50 +0200

    First Script

--- uitleg/uitleg.ino
+++ uitleg/uitleg.ino
@@ -1,9 +1,19 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  blink(1000, 11, 12);
+}
 
+void blink(int time, int pin1, int pin2){
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin2, HIGH);
+  delay(time);
+  digitalWrite(pin1, LOW);
+  digitalWrite(pin2, LOW);
+  delay(time);
 }
