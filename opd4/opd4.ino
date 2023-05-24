b3c27b8ed0937c99a9646f01c4bc852f438c73e7
Cybul <gabrielcybulski@gmail.com>
2023-05-24 15:23:00 +0200

    Forth Script

---
+++ opd4/opd4.ino
@@ -0,0 +1,34 @@
+// Led pins:
+const int ledPinArray[] = {13, 12, 11, 10, 9, 8, 7};
+
+// Potmeter
+const int potMeterPin = A5;
+int potMeterValue = 0; // Start value
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int pin = 6; pin >= 0; pin--) {
+    pinMode(ledPinArray[pin], OUTPUT);
+  }
+}
+
+void loop() {
+  potMeterValue = analogRead(potMeterPin);
+
+  int mapValue = map(potMeterValue, 0, 930, ledPinArray[0] + 1, ledPinArray[6]);
+
+  manageLights(mapValue);
+  Serial.println(potMeterValue);
+}
+
+void manageLights(int mapVal) {
+  for(int pin = ledPinArray[0]; pin >= ledPinArray[6]; pin--){
+    if(pin >= mapVal){
+    digitalWrite(pin, HIGH);
+    }
+    else{
+      digitalWrite(pin, LOW);
+    }
+  }
+}
