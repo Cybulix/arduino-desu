cdf0891fad652d71fef64ff8e917b73a9d7fbde3
Cybul <gabrielcybulski@gmail.com>
2023-05-24 15:51:13 +0200

    Fifth Script

---
+++ opd5/opd5.ino
@@ -0,0 +1,17 @@
+// Temp Meter
+const int tempMeterPin = A5;
+float temp;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  temp = analogRead(tempMeterPin);
+  // Get curr temp with use of map
+  temp = map(temp, 0, 1023, 0, 500);
+  Serial.print("Temperatuur in Celsius = ");
+  Serial.print(temp);
+  Serial.println();
+  delay(1000); // Get temp each second
+}
